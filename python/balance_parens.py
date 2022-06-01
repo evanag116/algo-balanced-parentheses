3674811e4c316cec3cf02ea910ff65165a74a316
Evan Garcia <evanag116@yahoo.com>
2022-05-31 21:45:42 -0500

    still not working. getting there.

--- python/balance_parens.py
+++ python/balance_parens.py
@@ -1,2 +1,37 @@
 def balance_parens(str):
-    pass
+    stack = []
+    symbols = {"(":")", ")":"(", "[":"]", "]":"[", "{":"}", "}":"{"}
+    ans = ""
+    indices = []
+
+    
+    for i, char in enumerate(str):
+        if char.isalnum():
+            indices.append(i)
+            continue
+        elif stack and char in symbols:
+            if stack[-1] == symbols[char]:
+                stack.pop()
+                indices.pop()
+            else:
+                stack.append(char)
+                indices.append(i)
+        else:
+            stack.append(char)
+            indices.append(i)
+        # print(stack)
+
+    for i in range(len(str)):
+        if i not in indices:
+            ans += str[i]
+    
+    
+    return stack
+
+print(balance_parens(")("))
+    
+
+# print(balance_parens("(()a))"))
+# print(balance_parens("(()()("))
+# print(balance_parens("abc(d)e(fgh))(i)j)k"))
+
